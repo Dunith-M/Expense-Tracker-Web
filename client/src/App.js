796eb8646e6a17db72a136d9aa6e697d4e2404d4
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-08-30 19:44:17 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-// File: src/App.js
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';   
-import Test from './pages/Test';   
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-
-          <Route path="/" element={<Home />} />   
-          <Route path="/test" element={<Test />} /> 
-
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
+import "../resources/default-layout.css";
 
 function DefaultLayout(props) {
   return (
-    <div classname='layout'>
+    <div classname="layout">
+      <div className="header d-flex justify-content-between align-items-center">
+        <h1 className="logo">Expense Tracker</h1>
 
-        <div className='header'>
-            <h1 className='logo'>Expense Tracker</h1>
+        <div>
+          <h1 classname="username">Username</h1>
         </div>
+      </div>
 
-        <div className='content'>
-            {props.children}
-        </div>
-      
+      <div className="content">{props.children}</div>
     </div>
-  )
+  );
 }
 
-export default DefaultLayout
+export default DefaultLayout;
